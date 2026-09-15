9bafddf1f39eef70c8c1be1901a20ddc504ce248
Fraj Yassine Lakhal <25002268+fyassine@users.noreply.github.com>
2026-09-15 13:26:32 +0200

    chore: delete old frontend

--- old_frontend/src/context/ColorModeContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const ColorModeContext = createContext({ toggleMode: () => {} });
-
-export function useColorMode() {
-  return useContext(ColorModeContext);
-}
